3cc3d4b7a09be1cc834df734cd861cf534ebfdc2
Garrett Estrin <garrett.estrin@gmail.com>
2017-06-25 21:07:43 -0700

    fixed port issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const
     express = require('express'),
     app = express(),
     path = require("path"),
-    PORT = 3000 || process.env.PORT
+    PORT = process.env.PORT || 3000
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.get('/', function(req, res){
