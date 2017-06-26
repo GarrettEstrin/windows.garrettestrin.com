88d59e381f8c383a32517fdf2d6af8fe8eefa041
Garrett Estrin <garrett.estrin@gmail.com>
2017-06-25 20:22:52 -0700

    set up grunt and sass

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,31 @@
-var FirstComponent = React.createClass({
-render: function(){
-    return (
-    <div className="icon-container">
-        <div className="icon">
-            <div className="text">Story <br/>Time</div>
-        </div>
-    </div>
-    )
-}
-});
+// right click context menu
+(function() {
 
-ReactDOM.render(
-<FirstComponent />,
-document.getElementById('app1')
-)
+  "use strict";
+
+  document.addEventListener( "contextmenu", function(e) {
+    console.log(e);
+  });
+
+})();
+
+// notification clock function
+function startTime() {
+    console.log("start Time")
+    var now = new Date();
+    var h = now.getHours();
+    var ampm = "AM";
+    if (h > 12){
+        h = h - 12
+        ampm = "PM";
+    }
+    var m = now.getMinutes();
+    if(m < 10){
+        m = "0" + m;
+    }
+    document.getElementById('time').innerHTML =
+    h + ":" + m + " " + ampm;
+    var t = setTimeout(startTime, 15000);
+}
+
+startTime();
