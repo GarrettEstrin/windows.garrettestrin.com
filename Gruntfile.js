42182cac8592559084fe6bd8f575d103525d1087
Garrett Estrin <garrett.estrin@gmail.com>
2017-09-17 22:12:53 -0700

    compressed js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,14 @@ module.exports = function(grunt) {
                 options: {
                     livereload: true
                 }
+            },
+            // concat: {
+            //     files: ['public/js/*.js'],
+            //     tasks: ['concat']
+            // }
+            uglify: {
+                files: ['public/js/*.js'],
+                tasks: ['uglify']
             }
         },
         sass: {
@@ -33,10 +41,32 @@ module.exports = function(grunt) {
                 ext: '.min.css'
               }]
             }
+          },
+          concat: {
+            dist: {
+                src: ['public/js/*.js', '!public/js/built.js'],
+                dest: 'public/js/built.js',
+              }
+          },
+          uglify: {
+              options: {
+
+              },
+            my_target: {
+              files: {
+                // 'public/js/components.min.js': ['public/js/components.js'],
+                'public/js/main.min.js': ['public/js/main.js'],
+                'public/js/react-dom.min.js': ['public/js/react-dom.js'],
+                'public/js/react.min.js': ['public/js/react.js'],
+                'public/js/browser.min.js': ['public/js/browser.min.js'],
+              }
+            }
           }
     });
     grunt.registerTask('default', ['watch']);
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 };
