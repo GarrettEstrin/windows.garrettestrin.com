7ebd0d0beb8cd23f2d820fe48bef6e6e49f96573
Garrett Estrin <garrett.estrin@gmail.com>
2017-09-17 21:27:57 -0700

    added server side compression

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const
     express = require('express'),
     app = express(),
     path = require("path"),
+    compression = require('compression'),
     PORT = process.env.PORT || 3000
 app.use(express.static('public'))
+// compress all responses
+app.use(compression())
 app.set('view engine', 'ejs')
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/public/views/index.html'))
