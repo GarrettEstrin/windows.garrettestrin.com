e54624f8662d22b561493ce878d0005693d458e2
Garrett Estrin <garrett.estrin@gmail.com>
2018-08-10 08:58:13 -0700

    adjusted startup time and rendering order

--- public/js/components.js
+++ public/js/components.js
@@ -238,88 +238,90 @@ var StartMenuWindow = React.createClass({
         )
     }
 });
-//   Render Community Story Time Icon
-ReactDOM.render(
-<App
-projectTitle="Community Story Time" 
-projectIcon="icon story-time-icon"
-projectSubtitle="A rails based web app that allows people to collaborate on stories."
-projectDescription="At community story time, users can start stories, continue previously started stories and make comments on stories."
-projectThumbnail="story-time thumbnail"
-projectCodeLink="https://github.com/GarrettEstrin/story_time"
-projectViewLink="http://st.garrettestrin.com/"/>,
-document.getElementById('app1')
-)
-// Render The Beer Journal Icon
-ReactDOM.render(
-<App
-projectTitle="The Beer Journal" 
-projectIcon="icon beer-journal-icon"
-projectSubtitle="A MEAN stack application where a user can log, review and share beers they have drunk"
-projectDescription="Users can create and login in to the web app and create and see beers that they have drunk."
-projectThumbnail="beer-journal thumbnail"
-projectCodeLink="https://github.com/GarrettEstrin/beer_web"
-projectViewLink="http://tbj.garrettestrin.com/"/>,
-document.getElementById('app2')
-)
-// Render Our Big Day in SF Icon
-ReactDOM.render(
-<App
-projectTitle="Our Big Day in SF" 
-projectIcon="icon big-day-icon"
-projectSubtitle="Interactive Wedding Website"
-projectDescription="Users can visit website for all wedding information and can fill out a form to inform bride and groom RSVP status."
-projectThumbnail="big-day thumbnail"
-projectCodeLink="https://github.com/GarrettEstrin/big_day"
-projectViewLink="http://ourbigdayinsf.com/"/>,
-document.getElementById('app3')
-)
-//   A word is worth one picture
-ReactDOM.render(
-<App
-projectTitle="A Word is Worth One Picture" 
-projectIcon="icon picture-game-icon"
-projectSubtitle="A node.js based web app where users can play a word and image based game."
-projectDescription="Users can create and login in to the web app and create see games that they are playing in."
-projectThumbnail="picture-game thumbnail"
-projectCodeLink="https://github.com/GarrettEstrin/Project-3"
-projectViewLink="https://infinite-eyrie-42957.herokuapp.com/"/>,
-document.getElementById('app4')
-)
+function renderDesktopIcons(){
+  //   Render Community Story Time Icon
+  ReactDOM.render(
+  <App
+  projectTitle="Community Story Time" 
+  projectIcon="icon story-time-icon"
+  projectSubtitle="A rails based web app that allows people to collaborate on stories."
+  projectDescription="At community story time, users can start stories, continue previously started stories and make comments on stories."
+  projectThumbnail="story-time thumbnail"
+  projectCodeLink="https://github.com/GarrettEstrin/story_time"
+  projectViewLink="http://st.garrettestrin.com/"/>,
+  document.getElementById('app1')
+  )
+  // Render The Beer Journal Icon
+  ReactDOM.render(
+  <App
+  projectTitle="The Beer Journal" 
+  projectIcon="icon beer-journal-icon"
+  projectSubtitle="A MEAN stack application where a user can log, review and share beers they have drunk"
+  projectDescription="Users can create and login in to the web app and create and see beers that they have drunk."
+  projectThumbnail="beer-journal thumbnail"
+  projectCodeLink="https://github.com/GarrettEstrin/beer_web"
+  projectViewLink="http://tbj.garrettestrin.com/"/>,
+  document.getElementById('app2')
+  )
+  // Render Our Big Day in SF Icon
+  ReactDOM.render(
+  <App
+  projectTitle="Our Big Day in SF" 
+  projectIcon="icon big-day-icon"
+  projectSubtitle="Interactive Wedding Website"
+  projectDescription="Users can visit website for all wedding information and can fill out a form to inform bride and groom RSVP status."
+  projectThumbnail="big-day thumbnail"
+  projectCodeLink="https://github.com/GarrettEstrin/big_day"
+  projectViewLink="http://ourbigdayinsf.com/"/>,
+  document.getElementById('app3')
+  )
+  //   A word is worth one picture
+  ReactDOM.render(
+  <App
+  projectTitle="A Word is Worth One Picture" 
+  projectIcon="icon picture-game-icon"
+  projectSubtitle="A node.js based web app where users can play a word and image based game."
+  projectDescription="Users can create and login in to the web app and create see games that they are playing in."
+  projectThumbnail="picture-game thumbnail"
+  projectCodeLink="https://github.com/GarrettEstrin/Project-3"
+  projectViewLink="https://infinite-eyrie-42957.herokuapp.com/"/>,
+  document.getElementById('app4')
+  )
 
-//   Guac-a-Mole!
-ReactDOM.render(
-<App
-projectTitle="Guac-a-Mole!" 
-projectIcon="icon mole-icon"
-projectSubtitle="A JavaScript based game."
-projectDescription="Simple game that allows a user to 'whack' as many moles as possible in the alloted time."
-projectThumbnail="mole-game thumbnail"
-projectCodeLink="https://github.com/GarrettEstrin/guac-a-mole"
-projectViewLink="https://garrettestrin.github.io/guac-a-mole/"/>,
-document.getElementById('app5')
-)
+  //   Guac-a-Mole!
+  ReactDOM.render(
+  <App
+  projectTitle="Guac-a-Mole!" 
+  projectIcon="icon mole-icon"
+  projectSubtitle="A JavaScript based game."
+  projectDescription="Simple game that allows a user to 'whack' as many moles as possible in the alloted time."
+  projectThumbnail="mole-game thumbnail"
+  projectCodeLink="https://github.com/GarrettEstrin/guac-a-mole"
+  projectViewLink="https://garrettestrin.github.io/guac-a-mole/"/>,
+  document.getElementById('app5')
+  )
 
-// Did the Ducks Win?
-ReactDOM.render(
-<App
-projectTitle="Did the Ducks Win?" 
-projectIcon="icon ducks-icon"
-projectSubtitle="DB Proof of Concept."
-projectDescription="Simple site that uses a Google Spreadsheet as a database"
-projectThumbnail="ducks thumbnail"
-projectCodeLink="https://github.com/GarrettEstrin/didtheduckswin.com"
-projectViewLink="http://didtheduckswin.com"/>,
-document.getElementById('app6')
-)
+  // Did the Ducks Win?
+  ReactDOM.render(
+  <App
+  projectTitle="Did the Ducks Win?" 
+  projectIcon="icon ducks-icon"
+  projectSubtitle="DB Proof of Concept."
+  projectDescription="Simple site that uses a Google Spreadsheet as a database"
+  projectThumbnail="ducks thumbnail"
+  projectCodeLink="https://github.com/GarrettEstrin/didtheduckswin.com"
+  projectViewLink="http://didtheduckswin.com"/>,
+  document.getElementById('app6')
+  )
 
-// Tic Tac Toe
-ReactDOM.render(
-<Application
-projectTitle="Tic-Tac-Toe" 
-projectIcon="icon ttt-icon"/>,
-document.getElementById('app7')
-)
+  // Tic Tac Toe
+  ReactDOM.render(
+  <Application
+  projectTitle="Tic-Tac-Toe" 
+  projectIcon="icon ttt-icon"/>,
+  document.getElementById('app7')
+  )
+}
 
 // About Start Menu Item
 ReactDOM.render(
@@ -426,26 +428,41 @@ function parseDataFromSpreadSheetAndFindSpecificMessage(data){
     if(a[i].gsx$a.$t == affiliateCode){
       var message = a[i].gsx$message.$t;
       globalMessage = message;
-      fadeOut(document.getElementById('jsStartUp'));
-      return setTimeout(function(){showStartUpMessage(message);}, 2500)
+      setTimeout(function(){
+        fadeOut(document.getElementById('jsStartUp'));
+      }, 3000)
+      setTimeout(function(){
+        renderDesktopIcons();
+      }, 4500)
+      setTimeout(function(){
+        showStartUpMessage(message);
+      }, 5500)
+        return;
     }
+    console.log()
     var message = "This website is designed to look like the classic theme from Microsoft Windows 95.  It is built using the React front end Javascript Framework as well as front end development tools such as Grunt and Sass.";
     globalMessage = message;
-    fadeOut(document.getElementById('jsStartUp'));
-    setTimeout(function(){showStartUpMessage(message)}, 2500)
+    setTimeout(function(){
+      fadeOut(document.getElementById('jsStartUp'));
+    }, 3000)
+    setTimeout(function(){
+      renderDesktopIcons();
+    }, 4500)
+    setTimeout(function(){
+      showStartUpMessage(message);
+    }, 5500)
   }
 }
 
 function fadeOut(el){
   el.style.opacity = 1;
   (function fade() {
-    if ((el.style.opacity -= .00699) < 0) {
+    if ((el.style.opacity -= .00499) < 0) {
       el.style.display = "none";
     } else {
       requestAnimationFrame(fade);
     }
   })();
 }
-
 getDataFromSpreadsheet();
 
